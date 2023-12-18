da5050326b8e813e997abf96442b22c4366f2209
bgocumasTIP <153921260+bgocumasTIP@users.noreply.github.com>
2023-12-18 20:21:58 +0800

    Update regressionapp.py

--- regressionapp.py
+++ regressionapp.py
@@ -21,16 +21,7 @@ def main():
     model_path = "regmodel_weights_best.hdf5"
     model = load_regression_model(model_path)
 
-    # Get user input for features
-    st.subheader("Input Features")
-    user_input = {}
-    for input_name in model.input_names:
-        user_input[input_name] = st.number_input(f"Enter {input_name}", value=0.0)
-
     # Make predictions
     if st.button("Predict"):
         prediction = predict_regression(model, user_input)
         st.success(f"Prediction: {prediction}")
-
-if __name__ == "__main__":
-    main()
